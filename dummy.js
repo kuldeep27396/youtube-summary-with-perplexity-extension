396e1cebb13dfe4b1e559697e56c924fe4cb19e7
Kuldeep Pal <kuldeep27396@gmail.com>
2025-07-26 15:38:15 +0530

    Create dummy.js

---
+++ dummy.js
@@ -0,0 +1,20 @@
+// Declare a variable
+let message = "Hello, JavaScript!";
+
+// Define a function that takes a name as an argument
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+// Call the function and store the result in a variable
+let greetingForUser = greet("User");
+
+// Log the variables to the console
+console.log(message);
+console.log(greetingForUser);
+
+// Demonstrate a simple arithmetic operation
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+console.log("The sum is: " + sum);
